e5ce79e0afa1fd932c126439227f197694b6cc98
AJ <anshul@hum22.com>
2020-08-19 19:40:03 +0800

    Add Support For iOS 11+

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "PayNowQRSwift",
     platforms: [
-        .macOS(.v10_14), .iOS(.v13)
+        .macOS(.v10_14), .iOS(.v11)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
